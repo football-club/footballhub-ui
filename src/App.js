a7de5d28360d5524b774b9b92fbe2a38f993b9ae
Tenzin Tashi <tenztashi8@gmail.com>
2023-03-09 22:44:33 -0500

    missed one file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import NavBar from "./components/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import News from "./components/news/News";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Predictions from "./pages/Predictions";
 import Fixtures from "./pages/Fixtures";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/fixture" element={<Fixtures />}></Route>
-        <Route path="/predictions" element={<Predictions />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/fixture" element={<Fixtures />}></Route>
+          <Route path="/predictions" element={<Predictions />}></Route>
         </Routes>
       </BrowserRouter>
     </>
